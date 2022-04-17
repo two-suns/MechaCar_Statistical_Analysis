533a1f96a2f6b1bc0e22f32ef8940ae9e485dedc
Bryan Eckard <ch13f_48@zoho.com>
2022-04-17 14:00:31 -0400

    Update with Deliverable 2 code.

---
+++ MechaCarChallenge.R
@@ -0,0 +1,7 @@
+MechaCar_mpg <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
+
+Suspension_Coil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
