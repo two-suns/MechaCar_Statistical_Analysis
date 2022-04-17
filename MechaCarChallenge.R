ca09b2834ec33433bc7372c2802a725de65d9f32
Bryan Eckard <ch13f_48@zoho.com>
2022-04-17 19:31:10 -0400

    Added comments to code.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,11 +1,14 @@
+#Perform linear regression and summary.
 MechaCar_mpg <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
 
+#Summarize PSI and group by lot.
 Suspension_Coil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
 total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
+#Perform t-test on PSI and then use subsets for each manufacturing lot.
 t.test(Suspension_Coil$PSI, mu=1500)
 
 lot1 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot1")
